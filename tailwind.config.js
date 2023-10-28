3067c680248baf5027da95687722e983e3b8e986
Code-Raiders <123230081+arjunxplorer@users.noreply.github.com>
2023-10-28 00:06:19 -0500

    test

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        'dark-black':'#14161b',
+        'dark-gray':'#1f242b',
+      }
+    },
+  },
+  plugins: [],
+}
+
